384d1032694f7eca1a71d75f53b9467971c91b5d
Panca Satrio Nugroho <100120868+panca-binaracademy@users.noreply.github.com>
2022-04-21 14:43:26 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Cara Clone Project
+
+1. git clone "https://github.com/Bimzking1/Binar-FEJS-Challange-4.git"
+2. masuk ke directory project
+3. npm install
+4. npm start
---
+++ src/Result/CarCard.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import CarDetail from './CarDetail'
+import CarImage from './CarImage'
+import './result.css'
+
+import IconPeople from "../Assets/fi_users.svg"
+import IconGear from "../Assets/fi_settings.svg"
+import IconCalendar from "../Assets/fi_calendar.svg"
+
+const CarCard = (props) => {
+  return (
+    <div className='car_card'>
+      <div className='car-image-container'>
+        <img src={props.car.image} className='car-image'/>
+      </div>
+      <div className='car-detail-container'>
+        <div className='car-name-type'>
+          {props.car.name}
+        </div>
+        <div className='car-price'>
+          Rp. {props.car.price} / hari
+        </div>
+        <div className='car-description'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+        <div className='car-detail-icons'>
+          <div className='car-detail-icon'>
+            <img src={IconPeople} className='car-icon'/>
+          </div>
+          <div className='car-detail-text'>
+            4 orang (ganti dari API)
+          </div>
+        </div>
+        <div className='car-detail-icons'>
+          <div className='car-detail-icon'>
+            <img src={IconGear} className='car-icon'/>
+          </div>
+          <div className='car-detail-text'>
+            Manual (ganti dari API)
+          </div>
+        </div>
+        <div className='car-detail-icons'>
+          <div className='car-detail-icon'>
+            <img src={IconCalendar} className='car-icon'/>
+          </div>
+          <div className='car-detail-text'>
+            Tahun 2020 (ganti dari API)
+          </div>
+        </div>
+        <button className='button-card-container'>
+          <div type="submit" className='button-card'>
+              Pilih Mobil
+          </div>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default CarCard
